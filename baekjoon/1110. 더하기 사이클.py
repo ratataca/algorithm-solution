9a5dc329a2373822f4b49ff15b227a2624b46a5f
ratataca <gywls18@koreatech.ac.kr>
2021-11-24 17:21:26 +0900

    add 21. 11. 24.

--- baekjoon/1110. 더하기 사이클.py
+++ baekjoon/1110. 더하기 사이클.py
@@ -1,24 +1,18 @@
-
 cnt = 0
 target = int(input())
 num = target
 
 while True:
+    # 각 자리수 더하기
+    ten, select_num1 = num // 10, num % 10
 
-    if num < 10:
-        num *= 10
-
-    ten, digit =  num // 10, num % 10
-    print("ten: {0}, digit: {1}".format(ten, digit))
-
-    tmp = (ten + digit) % 10
-    num = digit * 10 + tmp
-    print("tmp: {0}, num: {1}".format(tmp, num))
+    # 주어진 수의 가장 오른쪽 자리와 앞에서 구한 합의 가장 오른쪽 자리 수를 이어 붙임
+    select_num2 = (ten + select_num1) % 10
+    num = select_num1 * 10 + select_num2
     cnt += 1
 
+    # 원래 수로 돌아오면 CNT를 반환하라.
     if target == num:
         break
 
-    print(num)
-
-print(">>>", cnt)
+print(cnt)
