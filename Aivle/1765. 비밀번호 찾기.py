38182fac6550f060c3e72ec7eb60ce0c1ec2a537
ratataca <gywls18@koreatech.ac.kr>
2022-03-26 22:27:28 +0900

    2022. 03. 26.

---
+++ Aivle/1765. 비밀번호 찾기.py
@@ -0,0 +1,10 @@
+pw = input()
+
+result = ""
+for s in pw:
+    if s == 'c':
+        result += s
+        break
+    result += s
+
+print(result)
---
+++ Aivle/1770. 가장 큰 나머지.py
@@ -0,0 +1,3 @@
+N = int(input())
+
+print(max(N % 5, N % 7))
