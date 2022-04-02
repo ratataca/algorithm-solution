4ebf1a6bdd65611470fd8cd42614f28d44da575d
ratataca <gywls18@koreatech.ac.kr>
2022-04-02 12:20:05 +0900

    2022. 04. 02.

--- programmers/알고리즘/다익스트라/배달.py
+++ programmers/알고리즘/다익스트라/배달.py
@@ -9,14 +9,12 @@ def solution(N, road, K):
     for s_n, e_n, w in road:
         graph[s_n].append([e_n, w])
         graph[e_n].append([s_n, w])
-    print(graph)
-    
+
     # 방문 표시
     INF = int(1e9)
     distance = [INF] * (N + 1)
     start_node = 1
     
-
     # 다익스트라
     q = []
     distance[start_node] = 0
@@ -33,8 +31,12 @@ def solution(N, road, K):
             if cost <= K and cost < distance[e_n]:
                 distance[e_n] = cost
                 heapq.heappush(q, (cost, e_n))
-                answer += 1    
-    return answer + 1
+        
+    for n in distance:
+        if n != INF:
+            answer += 1
+
+    return answer
 
 
 N = 5
@@ -42,9 +44,13 @@ road = [[1,2,1],[2,3,3],[5,2,2],[1,4,2],[5,3,1],[5,4,2]]
 K = 3
 
 
-N = 6
-road = [[1,2,1],[1,3,2],[2,3,2],[3,4,3],[3,5,2],[3,5,3],[5,6,1]]
-K = 4
+# N = 6
+# road = [[1,2,1],[1,3,2],[2,3,2],[3,4,3],[3,5,2],[3,5,3],[5,6,1]]
+# K = 4
+
+# N = 2
+# road = [[1, 2, 4], [2, 1, 1]]
+# K = 10
 
 
 print("result : ", end='')
